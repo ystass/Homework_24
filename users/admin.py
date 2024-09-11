6f9e97376c590762b99ba34f5c906873915b32d5
ystass <stepanischevyy@mail.ru>
2024-09-11 20:06:44 +0400

    через админку наполнил платежи и с помощью фикстур наполнил базу (payments.json)

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User, Payments
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'phone', 'city')
+    list_filter = ('email', 'city')
+
+
+@admin.register(Payments)
+class UserPayments(admin.ModelAdmin):
+    list_display = ('id', 'user', 'date', 'course', 'lesson', 'payment_amount', 'payment_method')
+

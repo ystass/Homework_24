1ed4a152a504854891fb95351e4ee77035b4ed5d
ystass <stepanischevyy@mail.ru>
2024-09-13 18:14:40 +0400

    создал класс PaymentsListAPIView, прописал фильтрацию

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter, SearchFilter
 
-# Create your views here.
+from users.models import Payments
+from users.serialiser import PaymentsSerializer
+
+
+class PaymentsListAPIView(ListAPIView):
+    serializer_class = PaymentsSerializer
+    queryset = Payments.objects.all()
+    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+    filterset_fields = (
+        "course",
+        "lesson",
+    )
+    ordering_fields = ("date",)
+    search_fields = ("payment_method",)

0166c2c1e18f68719ea9d1d37b46eb0e9da14d43
ystass <stepanischevyy@mail.ru>
2024-09-13 18:10:22 +0400

    добавил "django_filters" в INSTALLED_APPS

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     "rest_framework",
     "users",
     "materials",
+
+    "django_filters",
 ]
 
 MIDDLEWARE = [
@@ -77,7 +79,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
-
+REST_FRAMEWORK = {
+     "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",)
+ }
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 

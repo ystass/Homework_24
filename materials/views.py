c189a4401dbcd2585e8edee854407686f7df2fa6
ystass <stepanischevyy@mail.ru>
2024-09-16 17:16:11 +0400

    автоматическое сохранение владельца при создании курса и урока

--- materials/views.py
+++ materials/views.py
@@ -6,6 +6,7 @@ from rest_framework.viewsets import ModelViewSet
 
 from materials.models import Course, Lesson
 from materials.serializer import CourseSerializer, LessonSerializer
+from users.permissions import IsModer, IsOwner
 
 
 class CourseViewSet(ModelViewSet):
@@ -17,11 +18,20 @@ class CourseViewSet(ModelViewSet):
         course.owner = self.request.user
         course.save()
 
+    def get_permissions(self):
+        if self.action == 'create':
+            self.permission_classes = (~IsModer, )
+        elif self.action in ['update', 'retrieve']:
+            self.permission_classes = (IsModer | IsOwner, )
+        elif self.action == 'destroy':
+            self.permission_classes = (IsOwner, )
+        return super().get_permissions()
+
 
 class LessonCreateAPIView(CreateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [~IsModer, IsAuthenticated]
 
     def perform_create(self, serializer):
         lesson = serializer.save()
@@ -37,16 +47,16 @@ class LessonListAPIView(ListAPIView):
 class LessonRetrieveAPIView(RetrieveAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsModer | IsOwner]
 
 
 class LessonUpdateAPIView(UpdateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsModer | IsOwner]
 
 
 class LessonDestroyAPIView(DestroyAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwner]

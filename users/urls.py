02fb13dfbecf5d3c65bd25b22be31e0541f18c80
ystass <stepanischevyy@mail.ru>
2024-09-16 15:08:06 +0400

    добавил настройки в REST_FRAMEWORK в для утентификации

--- users/urls.py
+++ users/urls.py
@@ -4,15 +4,15 @@ from rest_framework.routers import SimpleRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
-from users.views import PaymentsListAPIView
+from users.views import PaymentsListAPIView, UserViewSet
 
 app_name = UsersConfig.name
 router = SimpleRouter()
-#router.register("", UserViewSet, basename="users")
+router.register("", UserViewSet, basename="users")
 
 
 urlpatterns = [
-    path("payments/", PaymentsListAPIView.as_view(), name="payments"),
+    path("payments/", PaymentsListAPIView.as_view(permission_classes=(AllowAny,)), name="payments"),
     path(
         "login/",
         TokenObtainPairView.as_view(),
@@ -20,7 +20,7 @@ urlpatterns = [
     ),
     path(
         "token/refresh/",
-        TokenRefreshView.as_view(),
+        TokenRefreshView.as_view(permission_classes=(AllowAny,)),
         name="token_refresh",
     )
 ]

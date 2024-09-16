5f1d8dd96ccc39152269ac7bf49d15bde44961df
ystass <stepanischevyy@mail.ru>
2024-09-16 14:48:28 +0400

    сериализатор для User

--- users/serialiser.py
+++ users/serialiser.py
@@ -1,9 +1,17 @@
 from rest_framework.serializers import ModelSerializer
 
-from users.models import Payments
+from users.models import Payments, User
 
 
 class PaymentsSerializer(ModelSerializer):
     class Meta:
         model = Payments
+        fields = "__all__"
+
+
+class UserSerializer(ModelSerializer):
+    payments = PaymentsSerializer(source="payments_set", many=True, read_only=True)
+
+    class Meta:
+        model = User
         fields = "__all__"

4fed80e54c9584d9e480c0868ed0e32845bf15a7
ystass <stepanischevyy@mail.ru>
2024-09-17 16:13:17 +0400

    админка для курса и урока

--- materials/admin.py
+++ materials/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from materials.models import Course, Lesson
+
+
+@admin.register(Course)
+class UserPayments(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'owner')
+
+
+@admin.register(Lesson)
+class UserPayments(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'course', 'owner')

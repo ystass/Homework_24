f22c07b35c5f99574e0a85aef4fc85879d6050b2
ystass <stepanischevyy@mail.ru>
2024-09-18 16:19:24 +0400

    обновление зависимостей

--- users/views.py
+++ users/views.py
@@ -13,6 +13,11 @@ class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
     permission_classes = (AllowAny,)
 
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
+
 
 class PaymentsListAPIView(ListAPIView):
     serializer_class = PaymentsSerializer

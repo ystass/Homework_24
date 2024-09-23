37df5e46e1136ee7039d29e522809382580077eb
ystass <stepanischevyy@mail.ru>
2024-09-23 12:38:29 +0400

    добавил подписки в админку

--- materials/admin.py
+++ materials/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 
-from materials.models import Course, Lesson
+from materials.models import Course, Lesson, Subscription
 
 
 @admin.register(Course)
-class UserPayments(admin.ModelAdmin):
+class UserCourse(admin.ModelAdmin):
     list_display = ('id', 'title', 'description', 'owner')
 
 
 @admin.register(Lesson)
-class UserPayments(admin.ModelAdmin):
+class UserLesson(admin.ModelAdmin):
     list_display = ('id', 'title', 'description', 'course', 'owner')
+
+
+@admin.register(Subscription)
+class UserSubscription(admin.ModelAdmin):
+    list_display = ('id', 'course', 'user')

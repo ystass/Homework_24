b92587b61d470f30d4616f75554a8d7697007d31
ystass <stepanischevyy@mail.ru>
2024-09-26 11:37:53 +0400

    эндпоинт для создания платежа в stripe

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.permissions import AllowAny
@@ -6,6 +6,7 @@ from rest_framework.viewsets import ModelViewSet
 
 from users.models import Payments, User
 from users.serialiser import PaymentsSerializer, UserSerializer
+from users.services import create_stripe_product, create_stripe_price, create_stripe_session
 
 
 class UserViewSet(ModelViewSet):
@@ -29,3 +30,17 @@ class PaymentsListAPIView(ListAPIView):
     )
     ordering_fields = ("date",)
     search_fields = ("payment_method",)
+
+
+class PaymentsCreateAPIView(CreateAPIView):
+    serializer_class = PaymentsSerializer
+    queryset = Payments.objects.all()
+
+    def perform_create(self, serializer):
+        payment = serializer.save(user=self.request.user)
+        product_id = create_stripe_product(payment)
+        price_id = create_stripe_price(product_id, payment)
+        session_id, session_url = create_stripe_session(price_id)
+        payment.session_id = session_id
+        payment.link = session_url
+        payment.save()

1856a0f497aa39e689fba745cf2d9303e48527ba
ystass <stepanischevyy@mail.ru>
2024-10-02 16:25:11 +0400

    Реализация задачи по проверке последнего входа пользователей и отключение неактивных пользователей

--- materials/tasks.py
+++ materials/tasks.py
@@ -1,6 +1,8 @@
 from celery import shared_task
 from django.conf import settings
 from django.core.mail import send_mail
+from datetime import timedelta
+from django.utils import timezone
 
 from materials.models import Course, Subscription
 from users.models import User
@@ -8,7 +10,9 @@ from users.models import User
 
 @shared_task
 def update_notification(course_pk):
+
     """Отправка сообщения об обновлении курса по подписке"""
+
     course = Course.objects.filter(pk=course_pk).first()
     users = User.objects.all()
     for user in users:
@@ -20,3 +24,19 @@ def update_notification(course_pk):
                 from_email=settings.EMAIL_HOST_USER,
                 recipient_list=[user.email],
             )
+
+
+@shared_task
+def check_last_login():
+
+    """Проверка последнего входа пользователей и отключение неактивных пользователей"""
+
+    users = User.objects.filter(last_login__isnull=False)
+    today = timezone.now()
+    for user in users:
+        if today - user.last_login > timedelta(days=30):
+            user.is_active = False
+            user.save()
+            print(f'Пользователь {user.email} отключен')
+        else:
+            print(f'Пользователь {user.email} активен')
